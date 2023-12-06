e3198a8154d157eb0b34da11856c9959fe9d775c
Felipe Mallea <974598+llipe@users.noreply.github.com>
2023-12-06 00:24:33 -0300

    fix: wp-backup-to-s3 to get name from variable, check connectivity to S3 and work only on one wordpress path

--- scripts/wp-backup-to-s3.sh
+++ scripts/wp-backup-to-s3.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-# This script will backup all WordPress installations in a given directory to S3.
+# This script will backup the main WordPress folder to S3.
 # It will create a timestamped directory for each backup and upload it to S3.
-# It will also create a timestamped directory for each backup in /tmp and then
-# zip the WordPress files and database backup into a single file before uploading
-# it to S3. This is useful for large WordPress installations where the database
-# backup file is too large to upload to S3.
+# It will also create a timestamped directory in /tmp and then
+# zip the WordPress files before uploading it to S3.
 # 
 # Usage: wp-backup-to-s3.sh [options]
-# Example: wp-backup-to-s3.sh -p your_aws_profile -r your_aws_region -b your_s3_bucket -w /path/to/wordpress/installations
+# Example: wp-backup-to-s3.sh -r us-east-1 -b my-bucket -w /var/www/html -n my-wordpress
 
 # Set default values
-AWS_PROFILE="your_aws_profile"
 AWS_REGION="your_aws_region"
 S3_BUCKET="your_s3_bucket"
-WORDPRESS_PATH="/path/to/wordpress/installations"
+WORDPRESS_PATH="/path/to/wordpress"
+WP_NAME=""
 
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
-        -p|--profile)
-            AWS_PROFILE="$2"
-            shift
-            shift
-            ;;
         -r|--region)
             AWS_REGION="$2"
             shift
@@ -37,6 +30,11 @@ while [[ $# -gt 0 ]]; do
             ;;
         -w|--wordpress)
             WORDPRESS_PATH="$2"
+            shift
+            shift
+            ;;
+        -n|--name)
+            WP_NAME="$2"
             shift
             shift
             ;;
@@ -56,35 +54,33 @@ if [[ $1 == "-v" || $1 == "--version" ]]; then
     exit 0
 fi
 
-# Loop through each WordPress installation
-for WP_DIR in $WORDPRESS_PATH/*; do
-    if [[ -d "$WP_DIR" ]]; then
-        # Get the WordPress installation name
-        WP_NAME=$(basename "$WP_DIR")
+# Check S3 connectivity and permissions
+echo "Checking S3 connectivity and permissions..."
+aws s3 ls "s3://$S3_BUCKET" >/dev/null 2>&1 || { echo "Error: Unable to access S3 bucket. Please check your connectivity and permissions."; exit 1; }
+echo "S3 connectivity and permissions check passed."
 
-        # Create a timestamp for the backup
-        TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+# If WP_NAME is not provided, use the basename of WORDPRESS_PATH
+if [[ -z "$WP_NAME" ]]; then
+    WP_NAME=$(basename "$WORDPRESS_PATH")
+fi
 
-        # Create a directory for the backup
-        BACKUP_DIR="/tmp/$WP_NAME-$TIMESTAMP"
-        mkdir -p "$BACKUP_DIR"
+# Create a timestamp for the backup
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 
-        # Dump the WordPress database
-        echo "Dumping the WordPress database for $WP_NAME..."
-        wp --path="$WP_DIR" db export "$BACKUP_DIR/database.sql"
+# Create a directory for the backup
+BACKUP_DIR="/tmp/$WP_NAME-$TIMESTAMP"
+mkdir -p "$BACKUP_DIR"
 
-        # Zip the database backup and WordPress files
-        echo "Zipping the database backup and WordPress files for $WP_NAME..."
-        tar -czvf "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" -C "$WP_DIR" .
+# Zip the WordPress files
+echo "Zipping the WordPress files for $WP_NAME..."
+tar -czf "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" -C "$(dirname "$WORDPRESS_PATH")" "$(basename "$WORDPRESS_PATH")"
 
-        # Upload the backup to S3
-        echo "Uploading the backup to S3 for $WP_NAME..."
-        aws s3 cp "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" "s3://$S3_BUCKET/"
+# Upload the backup to S3
+echo "Uploading the backup to S3 for $WP_NAME..."
+aws s3 cp "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" "s3://$S3_BUCKET/"
 
-        # Clean up the temporary backup directory
-        echo "Cleaning up the temporary backup directory for $WP_NAME..."
-        rm -rf "$BACKUP_DIR"
+# Clean up the temporary backup directory
+echo "Cleaning up the temporary backup directory for $WP_NAME..."
+rm -rf "$BACKUP_DIR"
 
-        echo "Backup completed for $WP_NAME."
-    fi
-done
+echo "Backup completed for $WP_NAME."
