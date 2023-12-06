cbed6de4d2dcca77618eb4be8ff706e37d3bb1dc
Felipe Mallea <974598+llipe@users.noreply.github.com>
2023-12-06 00:28:08 -0300

    fix: add red coloring to errors on echo command

--- scripts/wp-backup-to-s3.sh
+++ scripts/wp-backup-to-s3.sh
@@ -14,6 +14,10 @@ S3_BUCKET="your_s3_bucket"
 WORDPRESS_PATH="/path/to/wordpress"
 WP_NAME=""
 
+# ANSI escape sequences for color formatting
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -39,7 +43,7 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         *)
-            echo "Unknown option: $1"
+            echo -e "${RED}Unknown option: $1${NC}"
             exit 1
             ;;
     esac
@@ -56,7 +60,7 @@ fi
 
 # Check S3 connectivity and permissions
 echo "Checking S3 connectivity and permissions..."
-aws s3 ls "s3://$S3_BUCKET" >/dev/null 2>&1 || { echo "Error: Unable to access S3 bucket. Please check your connectivity and permissions."; exit 1; }
+aws s3 ls "s3://$S3_BUCKET" >/dev/null 2>&1 || { echo -e "${RED}Error:${NC} Unable to access S3 bucket. Please check your connectivity and permissions."; exit 1; }
 echo "S3 connectivity and permissions check passed."
 
 # If WP_NAME is not provided, use the basename of WORDPRESS_PATH
@@ -77,7 +81,7 @@ tar -czf "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" -C "$(dirname "$WORDPRESS_PATH
 
 # Upload the backup to S3
 echo "Uploading the backup to S3 for $WP_NAME..."
-aws s3 cp "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" "s3://$S3_BUCKET/"
+aws s3 cp "$BACKUP_DIR/$WP_NAME-$TIMESTAMP.tar.gz" "s3://$S3_BUCKET/" || { echo -e "${RED}Error:${NC} Failed to upload the backup to S3."; exit 1; }
 
 # Clean up the temporary backup directory
 echo "Cleaning up the temporary backup directory for $WP_NAME..."
